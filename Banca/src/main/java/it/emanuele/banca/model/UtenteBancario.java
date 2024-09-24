8aa26607a8b9e1f465358f57bca97dbf17dc918a
Manuel <emanueledecicco97@gmail.com>
2024-09-24 18:54:58 +0200

    ottimizzazione codice

--- Banca/src/main/java/it/emanuele/banca/model/UtenteBancario.java
+++ Banca/src/main/java/it/emanuele/banca/model/UtenteBancario.java
@@ -13,75 +13,86 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table( name =  "utente_bancario")
+@Table(name = "utente_bancario")
 public class UtenteBancario {
 
-	
-	@Id
-	@GeneratedValue( strategy = GenerationType.IDENTITY)
-	private long id;
 
-	@Column ( name = "nome")
-	private String nome;
-	@Column ( name = "cognome")
-	private String cognome;
-	@Column ( name = "data_di_nascita")
-	private Date dataNascita;
-	@Column( name = "cf")
-	private String cf;
-	@Column(name = "attivo")
-	private boolean attivo = true;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "nome")
+    private String nome;
+    @Column(name = "cognome")
+    private String cognome;
+    @Column(name = "data_di_nascita")
+    private Date dataNascita;
+    @Column(name = "cf")
+    private String cf;
+    @Column(name = "attivo")
+    private boolean attivo = true;
 
 
-	@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
-	@JoinColumn(name="id")
-	
-	
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getCognome() {
-		return cognome;
-	}
-	public void setCognome(String cognome) {
-		this.cognome = cognome;
-	}
-	public Date getDataNascita() {
-		return dataNascita;
-	}
-	public void setDataNascita(Date dataNascita) {
-		this.dataNascita = dataNascita;
-	}
-	public String getCf() {
-		return cf;
-	}
-	public void setCf(String cf) {
-		this.cf = cf;
-	}
-	public boolean isAttivo() {
-		return attivo;
-	}
-	public void setAttivo(boolean attivo) {
-		this.attivo = attivo;
-	}
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "id")
 
-	@Override
-	public String toString() {
-		return "\nUtenteBancario{" +
-				"id=" + id +
-				", nome='" + nome + '\'' +
-				", cognome='" + cognome + '\'' +
-				", dataNascita=" + dataNascita +
-				", cf='" + cf + '\'' +
-				'}';
-	}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public Date getDataNascita() {
+        return dataNascita;
+    }
+
+    public void setDataNascita(Date dataNascita) {
+        this.dataNascita = dataNascita;
+    }
+
+    public String getCf() {
+        return cf;
+    }
+
+    public void setCf(String cf) {
+        this.cf = cf;
+    }
+
+    public boolean isAttivo() {
+        return attivo;
+    }
+
+    public void setAttivo(boolean attivo) {
+        this.attivo = attivo;
+    }
+
+    @Override
+    public String toString() {
+        return "\nUtenteBancario{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", cognome='" + cognome + '\'' +
+                ", dataNascita=" + dataNascita +
+                ", cf='" + cf + '\'' +
+                '}';
+    }
 }
